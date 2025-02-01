cc9d7f4ebd6b65b75f0384b0d0e813fff0fadcb6
kfarsoudi <farsoudi@gmail.com>
2025-01-31 16:06:40 -0800

    hw1p1

--- split.cpp
+++ split.cpp
@@ -13,11 +13,42 @@ the function below should be the only one in this file.
 #include "split.h"
 
 /* Add a prototype for a helper function here if you need */
+void split_recursive(Node*& in, Node*& odds, Node*& evens, Node*& evenHead, Node*& oddHead);
 
 void split(Node*& in, Node*& odds, Node*& evens)
 {
   /* Add code here */
-// WRITE YOUR CODE HERE
+    //Case 1: Empty List
+    if(in == nullptr){
+      return;
+    }
+    Node* evenHead = nullptr;
+    Node* oddHead = nullptr;
+    //Recursive Step
+    split_recursive(in, odds, evens, evenHead, oddHead);
+    //Clean-up dangling next pointers in certain scenarios:
+    if (odds != nullptr && odds->next != nullptr) {
+      odds->next = nullptr;
+    }
+    if (evens != nullptr && evens->next != nullptr) {
+      evens->next = nullptr;
+    }
+    odds = oddHead;
+    evens = evenHead;
+  //At the end: we don't want in anymore.
+  in=nullptr;
 }
 
 /* If you needed a helper function, write it here */
+void split_recursive(Node*& in, Node*& odds, Node*& evens, Node*& evenHead, Node*& oddHead) {
+  Node*& basis = (in->value % 2 == 0) ? evens : odds; //Checks if even, and operates on correct list.
+  if ( basis != nullptr) { //List already started
+    basis->next = in; //Add to next inlist
+  } else {
+    (&basis == &evens ? evenHead : oddHead) = in; // Else, start the list by pointing head
+  }
+  basis = in; //Update list pointer to new element
+  if ( in->next != nullptr) {
+    split_recursive(in->next, odds, evens, evenHead, oddHead);
+  }
+}
--- test_split.cpp
+++ test_split.cpp
@@ -10,8 +10,104 @@ g++ split.cpp test_split.cpp -o test_split
 */
 
 #include "split.h"
+#include <iostream>
+
+void printList(Node* head) {
+    if (head != nullptr) {
+        std::cout << head->value << " ";
+        if (head->next != nullptr) {
+            printList(head->next);
+        }
+    }
+}
+
+void freeList(Node* head) {
+    if (!head)
+        return;
+    freeList(head->next);
+    delete head;
+}
 
 int main(int argc, char* argv[])
 {
+    // Test case 1: Empty list
+    Node* in1 = nullptr;
+    Node* odds1 = nullptr;
+    Node* evens1 = nullptr;
+    split(in1, odds1, evens1);
+    std::cout << "Test case 1 -\nOdds: ";
+    printList(odds1);
+    std::cout << std::endl << "Evens: ";
+    printList(evens1);
+    std::cout << std::endl;
 
+    // Test case 2: Single element (odd)
+    Node* in2 = new Node(1, nullptr);
+    Node* odds2 = nullptr;
+    Node* evens2 = nullptr;
+    split(in2, odds2, evens2);
+    std::cout << "Test case 2 -\nOdds: ";
+    printList(odds2);
+    std::cout << std::endl << "Evens: ";
+    printList(evens2);
+    std::cout << std::endl;
+
+    // Test case 3: Single element (even)
+    Node* in3 = new Node(2, nullptr);
+    Node* odds3 = nullptr;
+    Node* evens3 = nullptr;
+    split(in3, odds3, evens3);
+    std::cout << "Test case 3 -\nOdds: ";
+    printList(odds3);
+    std::cout << std::endl << "Evens: ";
+    printList(evens3);
+    std::cout << std::endl;
+
+    // Test case 4: Multiple elements
+    Node* in4 = new Node(1, new Node(2, new Node(3, new Node(4, nullptr))));
+    Node* odds4 = nullptr;
+    Node* evens4 = nullptr;
+    split(in4, odds4, evens4);
+    std::cout << "Test case 4 -\nOdds: ";
+    printList(odds4);
+    std::cout << std::endl << "Evens: ";
+    printList(evens4);
+    std::cout << std::endl;
+
+    // Test case 5: All odds
+    Node* in5 = new Node(1, new Node(3, new Node(5, nullptr)));
+    Node* odds5 = nullptr;
+    Node* evens5 = nullptr;
+    split(in5, odds5, evens5);
+    std::cout << "Test case 5 -\nOdds: ";
+    printList(odds5);
+    std::cout << std::endl << "Evens: ";
+    printList(evens5);
+    std::cout << std::endl;
+
+    // Test case 6: All evens
+    Node* in6 = new Node(2, new Node(4, new Node(6, nullptr)));
+    Node* odds6 = nullptr;
+    Node* evens6 = nullptr;
+    split(in6, odds6, evens6);
+    std::cout << "Test case 6 -\nOdds: ";
+    printList(odds6);
+    std::cout << std::endl << "Evens: ";
+    printList(evens6);
+    std::cout << std::endl;
+
+    freeList(evens1);
+    freeList(odds1);
+    freeList(evens2);
+    freeList(odds2);
+    freeList(evens3);
+    freeList(odds3);
+    freeList(evens4);
+    freeList(odds4);
+    freeList(evens5);
+    freeList(odds5);
+    freeList(evens6);
+    freeList(odds6);
+
+    return 0;
 }
