327434f3bfab1c81c24017156b65ecdf56a6a522
kfarsoudi <farsoudi@gmail.com>
2025-01-31 18:54:06 -0800

    finished hw1

--- test_ulliststr.cpp
+++ test_ulliststr.cpp
@@ -5,9 +5,115 @@
 
 #include "ulliststr.h"
 
-//Use this file to test your ulliststr implementation before running the test suite
+using namespace std;
 
-int main(int argc, char* argv[])
-{
+// Helper functions to set up different scenarios
+ULListStr setup_empty_list() {
+  return ULListStr();
+}
 
+ULListStr setup_one_item_node() {
+  ULListStr dat;
+  dat.push_back("1");
+  return dat;
+}
+
+ULListStr setup_multiple_item_nodes() {
+  ULListStr dat;
+  for (int i = 1; i <= ARRSIZE + 2; ++i) {
+    dat.push_back(to_string(i));
+  }
+  return dat;
+}
+
+// Test functions
+void test_push_back(ULListStr& dat) {
+  dat.push_back("new");
+  cout << "test push_back: ";
+  for (size_t i = 0; i < dat.size(); ++i) {
+    cout << dat.get(i) << " ";
+  }
+  cout << endl;
+}
+
+void test_push_front(ULListStr& dat) {
+  dat.push_front("new");
+  cout << "test push_front: ";
+  for (size_t i = 0; i < dat.size(); ++i) {
+    cout << dat.get(i) << " ";
+  }
+  cout << endl;
+}
+
+void test_pop_back(ULListStr& dat) {
+  dat.pop_back();
+  cout << "test pop_back: ";
+  for (size_t i = 0; i < dat.size(); ++i) {
+    cout << dat.get(i) << " ";
+  }
+  cout << endl;
+}
+
+void test_pop_front(ULListStr& dat) {
+  dat.pop_front();
+  cout << "test pop_front: ";
+  for (size_t i = 0; i < dat.size(); ++i) {
+    cout << dat.get(i) << " ";
+  }
+  cout << endl;
+}
+
+void test_back(ULListStr& dat) {
+  cout << "test back: " << dat.back() << endl;
+}
+
+void test_front(ULListStr& dat) {
+  cout << "test front: " << dat.front() << endl;
+}
+
+void test_size(ULListStr& dat) {
+  cout << "test size: " << dat.size() << endl;
+}
+
+void test_clear(ULListStr& dat) {
+  dat.clear();
+  cout << "test clear: " << dat.size() << endl;
+}
+
+int main(int argc, char* argv[]) {
+//   // Test with empty list
+//   ULListStr empty_list = setup_empty_list();
+//   cout << "Testing with empty list:" << endl;
+//   test_push_back(empty_list);
+//   test_push_front(empty_list);
+//   test_pop_back(empty_list);
+//   test_pop_front(empty_list);
+//   test_size(empty_list);
+//   test_clear(empty_list);
+
+  // Test with one item node
+  ULListStr one_item_node = setup_one_item_node();
+  cout << "Testing with one item node:" << endl;
+  test_push_back(one_item_node);
+  test_push_front(one_item_node);
+  test_pop_back(one_item_node);
+  test_pop_front(one_item_node);
+  test_back(one_item_node);
+  test_front(one_item_node);
+  test_size(one_item_node);
+  test_clear(one_item_node);
+
+  // Test with multiple item nodes
+  ULListStr multiple_item_nodes = setup_multiple_item_nodes();
+  cout << "Testing with multiple item nodes:" << endl;
+  test_push_back(multiple_item_nodes);
+  test_push_front(multiple_item_nodes);
+  test_pop_back(multiple_item_nodes);
+  test_pop_front(multiple_item_nodes);
+  test_back(multiple_item_nodes);
+  test_front(multiple_item_nodes);
+  test_size(multiple_item_nodes);
+  test_clear(multiple_item_nodes);
+
+  return 0;
 }
--- ulliststr.cpp
+++ ulliststr.cpp
@@ -25,6 +25,128 @@ size_t ULListStr::size() const
 }
 
 // WRITE YOUR CODE HERE
+void ULListStr::push_back(const std::string& val) {
+  //Empty list:
+  if (empty()) {
+    tail_ = head_ = new Item();
+    head_->first = 0;
+    head_->last = 1;
+    head_->val[0] = val;
+    size_++;
+    return;
+  } else if (tail_->last < ARRSIZE) { //Space in tail
+    tail_->val[tail_->last] = val;
+    tail_->last++; size_++;
+    return;
+  } else {
+    Item* newTail = new Item();
+    newTail->first = 0;
+    newTail->last = 1;
+    newTail->val[0] = val;
+    newTail->prev = tail_;
+    tail_->next = newTail;
+    tail_ = newTail;
+    size_++;
+    return;
+  }
+}
+
+void ULListStr::pop_back() {
+  if (empty()) {
+    return;
+  } else if (size() == 1) {                    //Single Element
+    delete head_;
+    head_=tail_=nullptr;
+  } else if (tail_->first+1 != tail_->last) {  //Multiple Elements in tail
+    tail_->last--;
+  } else {                                     //Single Element in tail
+    Item* prev = tail_->prev;
+    delete tail_;
+    tail_ = prev;
+    tail_->next = nullptr;
+  }
+
+  size_--;
+}
+
+void ULListStr::push_front(const std::string& val) {
+  if (empty()) {                        //Empty
+    tail_ = head_ = new Item();
+    head_->first = 0;
+    head_->last = 1;
+    head_->val[0] = val;
+    size_++;
+    return;
+  } else if (head_->first > 0) {   //Head has space
+    head_->val[head_->first-1] = val;
+    head_->first--; size_++;
+  } else {                              //Head is full
+    Item* newHead = new Item();
+    newHead->first = ARRSIZE - 1;
+    newHead->last = ARRSIZE;
+    newHead->val[newHead->first] = val;
+    newHead->next = head_;
+    head_->prev = newHead;
+    head_ = newHead;
+    size_++;
+    return;
+  }
+}
+
+void ULListStr::pop_front() {
+  if (empty()) {                                  //Empty
+    return;
+  } else if (size() == 1) {                       //Single element
+    delete head_;
+    head_=tail_=nullptr;
+  } else if (head_->first+1 != head_->last) {     //Multiple in head
+    head_->first++;
+  } else {                                        //Single element in head
+    Item* next = head_->next;
+    delete head_;
+    head_=next;
+    head_->prev = nullptr;
+  }
+
+  size_--;
+}
+
+
+
+std::string const & ULListStr::back() const {
+  if (empty()) {
+    return nullptr;
+  }
+  return tail_->val[tail_->last - 1];
+}
+
+std::string const & ULListStr::front() const {
+  if (empty()) {
+    return nullptr;
+  }
+  return head_->val[head_->first];
+}
+
+std::string* ULListStr::getValAtLoc(size_t loc) const {
+  if (loc >= size_) {
+    return nullptr;
+  }
+  Item* curr = head_;
+  size_t index = 0;
+
+  while (curr != nullptr) {
+
+    size_t currSize = curr->last - curr->first;
+
+    if ((loc >= index) && (loc < index+currSize)) {
+      return &(curr->val[curr->first + (loc-index)]);
+    }
+
+    index += currSize;
+    curr = curr->next;
+  }
+  return nullptr;
+}
 
 void ULListStr::set(size_t loc, const std::string& val)
 {
